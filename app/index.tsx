e16e326e21eccb89be900121a3bded660cedb9d0
EthanSantos <ethanangelo61@gmail.com>
2024-12-14 00:08:45 -0800

    New Fitness Project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import "../global.css"
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-gray-100">
+      <Text className="text-lg font-bold text-blue-500">Hello NativeWind!</Text>
+    </View>
+  );
+}
