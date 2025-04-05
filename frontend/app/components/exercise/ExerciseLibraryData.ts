5e270d70eb9df6bc5bba188507807024ba74d1e5
EthanSantos <ethanangelo61@gmail.com>
2025-04-04 23:07:02 -0700

    updated exercise library

--- frontend/app/components/exercise/ExerciseLibraryData.ts
+++ frontend/app/components/exercise/ExerciseLibraryData.ts
@@ -262,5 +262,235 @@ export const exerciseLibraryData: ExerciseData[] = [
         id: '49',
         exercise_name: 'Ab Rollout',
         muscle_groups: 'Core',
+    },
+    {
+        id: '50',
+        exercise_name: 'Pendulum Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '51',
+        exercise_name: 'Meadows Row',
+        muscle_groups: 'Back, Biceps',
+    },
+    {
+        id: '52',
+        exercise_name: 'Hack Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '53',
+        exercise_name: 'Sissy Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '54',
+        exercise_name: 'Bulgarian Split Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '55',
+        exercise_name: 'Chest-Supported Row',
+        muscle_groups: 'Back, Biceps',
+    },
+    {
+        id: '56',
+        exercise_name: 'Incline Smith Machine Press',
+        muscle_groups: 'Chest, Triceps',
+    },
+    {
+        id: '57',
+        exercise_name: 'Landmine Press',
+        muscle_groups: 'Shoulders, Chest',
+    },
+    {
+        id: '58',
+        exercise_name: 'Pullovers',
+        muscle_groups: 'Back, Chest',
+    },
+    {
+        id: '59',
+        exercise_name: 'Viking Press',
+        muscle_groups: 'Shoulders, Triceps',
+    },
+    {
+        id: '60',
+        exercise_name: 'Belt Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '61',
+        exercise_name: 'Cross-Body Hammer Curl',
+        muscle_groups: 'Biceps',
+    },
+    {
+        id: '62',
+        exercise_name: 'Rack Pulls',
+        muscle_groups: 'Back, Legs',
+    },
+    {
+        id: '63',
+        exercise_name: 'Cable Pullthrough',
+        muscle_groups: 'Legs, Back',
+    },
+    {
+        id: '64',
+        exercise_name: 'Adductor Machine',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '65',
+        exercise_name: 'Reverse Pec Deck',
+        muscle_groups: 'Shoulders, Back',
+    },
+    {
+        id: '66',
+        exercise_name: 'Decline Bench Press',
+        muscle_groups: 'Chest, Triceps',
+    },
+    {
+        id: '67',
+        exercise_name: 'Smith Machine Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '68',
+        exercise_name: 'Smith Machine Calf Raise',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '69',
+        exercise_name: 'Machine Chest Press',
+        muscle_groups: 'Chest, Triceps',
+    },
+    {
+        id: '70',
+        exercise_name: 'Kneeling Cable Crunch',
+        muscle_groups: 'Core',
+    },
+    {
+        id: '71',
+        exercise_name: 'Hanging Leg Raise',
+        muscle_groups: 'Core',
+    },
+    {
+        id: '72',
+        exercise_name: 'Decline Cable Pullover',
+        muscle_groups: 'Back, Chest',
+    },
+    {
+        id: '73',
+        exercise_name: 'Dumbbell Pullover',
+        muscle_groups: 'Back, Chest',
+    },
+    {
+        id: '74',
+        exercise_name: 'Z-Press',
+        muscle_groups: 'Shoulders, Triceps',
+    },
+    {
+        id: '75',
+        exercise_name: 'Single-Arm Dumbbell Row',
+        muscle_groups: 'Back, Biceps',
+    },
+    {
+        id: '76',
+        exercise_name: 'Decline Push-Up',
+        muscle_groups: 'Chest, Triceps',
+    },
+    {
+        id: '77',
+        exercise_name: 'Incline Push-Up',
+        muscle_groups: 'Chest, Triceps, Shoulders',
+    },
+    {
+        id: '78',
+        exercise_name: 'Straight Arm Pulldown',
+        muscle_groups: 'Back',
+    },
+    {
+        id: '79',
+        exercise_name: 'Standing Cable Row',
+        muscle_groups: 'Back, Biceps',
+    },
+    {
+        id: '80',
+        exercise_name: 'Goblet Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '81',
+        exercise_name: 'Cable Lateral Raise',
+        muscle_groups: 'Shoulders',
+    },
+    {
+        id: '82',
+        exercise_name: 'Banded Pull-Apart',
+        muscle_groups: 'Shoulders, Back',
+    },
+    {
+        id: '83',
+        exercise_name: 'Barbell Curl',
+        muscle_groups: 'Biceps',
+    },
+    {
+        id: '84',
+        exercise_name: 'Decline Skull Crusher',
+        muscle_groups: 'Triceps',
+    },
+    {
+        id: '85',
+        exercise_name: 'JM Press',
+        muscle_groups: 'Triceps',
+    },
+    {
+        id: '86',
+        exercise_name: 'Spider Curl',
+        muscle_groups: 'Biceps',
+    },
+    {
+        id: '87',
+        exercise_name: 'Bayesian Curl',
+        muscle_groups: 'Biceps',
+    },
+    {
+        id: '88',
+        exercise_name: 'Sissy Hack Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '89',
+        exercise_name: 'Toes-to-Bar',
+        muscle_groups: 'Core',
+    },
+    {
+        id: '90',
+        exercise_name: 'L-Sit',
+        muscle_groups: 'Core',
+    },
+    {
+        id: '91',
+        exercise_name: 'Cable Crunch',
+        muscle_groups: 'Core',
+    },
+    {
+        id: '92',
+        exercise_name: 'Seal Row',
+        muscle_groups: 'Back, Biceps',
+    },
+    {
+        id: '93',
+        exercise_name: 'Staggered Stance Smith Machine Squat',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '94',
+        exercise_name: 'Belt Squat March',
+        muscle_groups: 'Legs',
+    },
+    {
+        id: '95',
+        exercise_name: 'Lying Tricep Extension',
+        muscle_groups: 'Triceps',
     }
 ];
